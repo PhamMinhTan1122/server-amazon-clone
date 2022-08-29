e07fb3260d2d4e1d8b21bf6716e7f352bedc43e3
Phạm Minh Tấn <85481444+PhamMinhTan1122@users.noreply.github.com>
2022-08-29 13:25:24 +0700

    ad cors into file index

--- index.js
+++ index.js
@@ -6,6 +6,14 @@ const mongoose = require('mongoose');
 const authRouter = require('./routers/auth');
 const DB = "mongodb+srv://minhtan:masterminhtan@cluster0.lczbvig.mongodb.net/?retryWrites=true&w=majority"
 //INIT
+const cors=require("cors");
+const corsOptions ={
+   origin:'*', 
+   credentials:true,            //access-control-allow-credentials:true
+   optionSuccessStatus:200,
+}
+
+app.use(cors(corsOptions))
 
 
 const PORT = process.env.PORT || 5000;
