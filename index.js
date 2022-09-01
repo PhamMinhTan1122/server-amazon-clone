5c5fdcedcdc0abf29515c91acedcd1684025d431
Phạm Minh Tấn <85481444+PhamMinhTan1122@users.noreply.github.com>
2022-09-01 14:08:22 +0700

    tuy chinh giao dien

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 //IMPORT PACKAGE
 const express = require('express');
 const mongoose = require('mongoose');
+const io = require('socket.io')
 //IMPORT FROM OTHER FILES
 const authRouter = require('./routers/auth');
 const DB = "mongodb+srv://minhtan:masterminhtan@cluster0.lczbvig.mongodb.net/?retryWrites=true&w=majority"
 //INIT
 const cors=require("cors");
+
 const corsOptions ={
    origin:'*', 
    credentials:true,            //access-control-allow-credentials:true
@@ -16,10 +18,10 @@ const app = express();
 app.use(cors(corsOptions))
 
 
+
 const PORT = process.env.PORT || 5000;
-// app.use(
-//     express.urlencoded({ extended: true })
-// );
+ 
+
 app.use(express.json());
 app.use(authRouter);
 mongoose.connect(DB).then(()=> {
